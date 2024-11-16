01b186615ef6da7fccd79d6b5fe6be355c9f3a9f
samuelselasi <samelsekasi@gmail.com>
2024-11-16 19:21:49 +0000

    Add main routers

--- payroll_management/src/app/main.py
+++ payroll_management/src/app/main.py
@@ -7,6 +7,10 @@ from app.routers.bonustype import main as bonustype
 from app.routers.deducttype import main as deducttype
 from app.routers.paytype import main as paytype
 from app.routers.holiday import main as holiday
+from app.routers.sickleave import main as sickleave
+from app.routers.payhistory import main as payhistory
+from app.routers.bonus import main as bonus
+from app.routers.deduction import main as deduction
 
 app = FastAPI(debug=True)
 
@@ -15,6 +19,10 @@ app.include_router(bonustype.router, tags=["Bonus Types"])
 app.include_router(deducttype.router, tags=["Deduct Types"])
 app.include_router(paytype.router, tags=["Pay Types"])
 app.include_router(holiday.router, tags=["Holidays"])
+app.include_router(sickleave.router, tags=["Sick Leaves"])
+app.include_router(payhistory.router, tags=["Pay History"])
+app.include_router(bonus.router, tags=["Bonuses"])
+app.include_router(deduction.router, tags=["Deductions"])
 
 @app.get("/")
 async def root():
--- payroll_management/src/main.py
+++ payroll_management/src/main.py
@@ -2,7 +2,8 @@
 """Module to initialize routers and endpoints"""
 
 from fastapi import FastAPI
-from app import employees, bonustype, deducttype, paytype, holiday
+from app import (employees, bonustype, deducttype, paytype,
+        holiday, sickleave, payhistory, bonus, deduction)
 import uvicorn
 
 app = FastAPI(debug=True)
@@ -12,6 +13,10 @@ app.include_router(bonustype.router, tags=["Bonus Types"])
 app.include_router(deducttype.router, tags=["Deduct Types"])
 app.include_router(paytype.router, tags=["Pay Types"])
 app.include_router(holiday.router, tags=["Holidays"])
+app.include_router(sickleave.router, tags=["Sick Leaves"])
+app.include_router(payhistory.router, tags=["Pay History"])
+app.include_router(bonus.router, tags=["Bonuses"])
+app.include_router(deduction.router, tags=["Deductions"])
 
 @app.get("/")
 async def root():
