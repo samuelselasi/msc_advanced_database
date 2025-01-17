606fcf9bea90ffdf5062dd6f3a5924d4cc7ce97a
samuelselasi <samelsekasi@gmail.com>
2025-01-17 11:15:05 +0000

    Add sql scripts to create db and tables

---
+++ census_project/database/create_db.sql
@@ -0,0 +1,14 @@
+-- Database: census
+
+-- DROP DATABASE IF EXISTS census;
+
+CREATE DATABASE census
+    WITH
+    OWNER = admin
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
